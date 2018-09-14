dd5b84493f15a5a01487ea97ee06247f0cc7c9bb
Anton Miroshkin <anton.miroshkin@gmail.com>
2018-09-14 18:25:00 +0300

    Add test to Point class

---
+++ TreehouseDefenseTests/PointTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using TreehouseDefense;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense.Tests
+{
+    public class PointTests
+    {
+        [Fact()]
+        public void PointTest()
+        {
+            int x = 5;
+            int y = 6;
+
+            var point = new Point(x,y);
+
+            Assert.Equal(x, point.X);
+            Assert.Equal(y, point.Y);
+        }
+
+        [Fact()]
+        public void DistanceToTest()
+        {
+            var point = new Point(3,4);
+            var target = new Point(0,0);
+
+            var expected = 5.0;
+            var actual = target.DistanceTo(point);
+
+            Assert.Equal(expected, actual, 2);
+        }
+    }
+}
