62eb8a6089d9c58d4ae4e9abdbce4ce820898441
Anton Miroshkin <anton.miroshkin@gmail.com>
2018-09-14 18:39:15 +0300

    Add map location tests

--- TreehouseDefenseTests/PointTests.cs
+++ TreehouseDefenseTests/PointTests.cs
@@ -23,12 +23,24 @@ namespace TreehouseDefense.Tests
         }
 
         [Fact()]
-        public void DistanceToTest()
+        public void DistanceToPathagoreanTriple()
         {
             var point = new Point(3,4);
             var target = new Point(0,0);
 
             var expected = 5.0;
+            var actual = target.DistanceTo(point);
+
+            Assert.Equal(expected, actual, 2);
+        }
+
+        [Fact]
+        public void DistanceToPointAtSamePosition()
+        {
+            var point = new Point(3, 4);
+            var target = new Point(3, 4);
+
+            var expected = 0.0;
             var actual = target.DistanceTo(point);
 
             Assert.Equal(expected, actual, 2);
